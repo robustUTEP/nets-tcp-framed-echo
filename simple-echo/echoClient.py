fd1b5e5236d2d08d53e8641eb1cc41e57aa80c2d
Eric Freudenthal <eric@freudenthal.net>
2018-09-18 03:09:19 -0600

    modified simple-echo for py3; also now uses params pkg

--- simple-echo/echoClient.py
+++ simple-echo/echoClient.py
@@ -1,51 +1,72 @@
-# Echo client program
-import socket
-import sys
+#! /usr/bin/env python3
 
-serverHost = 'localhost'
-serverPort = 50006
+# Echo client program
+import socket, sys, re
+sys.path.append("../lib")       # for params
+import params
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage  = paramMap["server"], paramMap["usage"]
+
+if usage:
+    params.usage()
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
 
 s = None
 for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
     af, socktype, proto, canonname, sa = res
     try:
-        print "creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto)
+        print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
         s = socket.socket(af, socktype, proto)
-    except socket.error, msg:
-        print " error: %s" % msg
+    except socket.error as msg:
+        print(" error: %s" % msg)
         s = None
         continue
     try:
-        print " attempting to connect to %s" % repr(sa)
+        print(" attempting to connect to %s" % repr(sa))
         s.connect(sa)
-    except socket.error, msg:
-        print " error: %s" % msg
+    except socket.error as msg:
+        print(" error: %s" % msg)
         s.close()
         s = None
         continue
     break
 
 if s is None:
-    print 'could not open socket'
+    print('could not open socket')
     sys.exit(1)
 
 outMessage = "Hello world!"
 
-print "sending '%s'" % outMessage
-s.send(outMessage)
+print("sending '%s'" % outMessage)
+s.send(outMessage.encode())
 
-data = s.recv(1024)
-print "Received '%s'" % data
+data = s.recv(1024).decode()
+print("Received '%s'" % data)
 
-print "sending '%s'" % outMessage
-s.send(outMessage)
+print("sending '%s'" % outMessage)
+s.send(outMessage.encode())
 
 s.shutdown(socket.SHUT_WR)      # no more output
 
 while 1:
-    data = s.recv(1024)
-    print "Received '%s'" % data
+    data = s.recv(1024).decode()
+    print("Received '%s'" % data)
     if len(data) == 0:
         break
-print "Zero length read.  Closing"
+print("Zero length read.  Closing")
 s.close()
--- simple-echo/echoServer.py
+++ simple-echo/echoServer.py
@@ -1,21 +1,39 @@
-# Echo server program
-import socket
+#! /usr/bin/env python3
 
-clientHost = ''                 # Symbolic name meaning all available interfaces
-clientPort = 50006              # Arbitrary non-privileged port
+# Echo server program
+
+import socket, sys, re
+sys.path.append("../lib")       # for params
+import params
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+listenPort = paramMap['listenPort']
+listenAddr = ''       # Symbolic name meaning all available interfaces
+
+if paramMap['usage']:
+    params.usage()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((clientHost, clientPort))
+s.bind((listenAddr, listenPort))
 s.listen(1)              # allow only one outstanding request
 # s is a factory for connected sockets
 
 conn, addr = s.accept()  # wait until incoming connection request (and accept it)
-print 'Connected by', addr
+print('Connected by', addr)
 while 1:
-    data = conn.recv(1024)
+    data = conn.recv(1024).decode()
     if not data: break
     sendMsg = "Echoing %s" % data
-    print "Received '%s', sending '%s'" % (data, sendMsg)
-    conn.send(sendMsg)
+    print("Received '%s', sending '%s'" % (data, sendMsg))
+    conn.send(sendMsg.encode())
 conn.close()
 
