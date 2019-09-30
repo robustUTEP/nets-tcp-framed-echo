c889f80db95f7358399e9e4b418edc5f2e5f326f
Daniel Cervantes <drcervantes@miners.utep.edu>
2019-09-30 15:52:04 -0600

    fixed imports

--- framed-echo/framedForkServer.py
+++ framed-echo/framedForkServer.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python3
 
+import sys
 sys.path.append("../lib")       # for params
-
-import sys, os, socket, params
-
+import re, socket, params, os
 
 switchesVarDefaults = (
     (('-l', '--listenPort') ,'listenPort', 50001),
